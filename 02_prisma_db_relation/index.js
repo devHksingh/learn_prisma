592239912ee59cd15943a92b6e7a9cd081796d22
devHksingh <hksingh.dev@gmail.com>
2024-10-01 19:00:59 +0530

    index,js setup

--- 02_prisma_db_relation/index.js
+++ 02_prisma_db_relation/index.js
@@ -1,7 +1,22 @@
 import express from 'express'
+import cookieparser from 'cookie-parser'
+import dotenv from "dotenv"
+
+dotenv.config({
+    path:'./.env'
+})
 
 const app = express()
 
+// regular middleware
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-app.listen(3001,()=>console.log(`Server is running on port ${3001}`))
+// cookie middleware
+app.use(cookieparser())
+
+app.get('/',(req,res)=>{
+    res.send('Learning prisma with mongodb')
+})
+
+app.listen(3000,()=>console.log(`Server is running on port ${3000}`))
