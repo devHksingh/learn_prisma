090a1a1ffc8f0314154922ce5f3a70e63ead1544
devHksingh <hksingh.dev@gmail.com>
2024-10-03 08:31:37 +0530

    add api for errors

---
+++ 03_test_todo_mongo/src/utils/ApiError.ts
@@ -0,0 +1,29 @@
+
+
+
+class ApiError extends Error {
+    statusCode: Number;
+    data: null;
+    success: boolean;
+    errors: any[];
+    
+    constructor(
+        statusCode:Number,
+        message = "Something went wrong",
+        errors = [],
+        stack =''
+    ){
+        super(message);
+        this.statusCode = statusCode
+        this.data = null
+        this.message =message
+        this.success = false
+        this.errors = errors
+
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
